5d8a42641667747ba4bb1a02a44f63f1d7c7155c
Thorsten Wagner <thotho@posteo.de>
2018-01-03 14:45:33 +0100

    Read the image depth from the frist image

--- train.py
+++ train.py
@@ -44,6 +44,7 @@ The configuration file is a json file and looks like this:
 import argparse
 import os
 import numpy as np
+import cv2
 from preprocessing import parse_annotation
 from frontend import YOLO
 import json
@@ -93,6 +94,16 @@ def _main_(args):
     
     overlap_labels = set(config['model']['labels']).intersection(set(train_labels.keys()))
 
+     # Read first image and check the image depth. Give a warning if image depth and depth given in annotation is different 
+    img_first = cv2.imread(train_imgs[0]['filename'])
+    isgrey = np.all(img_first[:,:,0] == img_first[:,:,1]) and  np.all(img_first[:,:,0] == img_first[:,:,2])
+    if isgrey and depth==3:
+        print "Annotation image depth does not match actual image depth. Use actual image depth"
+        depth = 1
+    if isgrey == false and depth == 1
+        print "Annotation image depth does not match actual image depth. Use actual image depth"
+        depth = 3
+    
     print 'Seen labels:\t', train_labels
     print 'Given labels:\t', config['model']['labels']
     print 'Overlap labels:\t', overlap_labels    
