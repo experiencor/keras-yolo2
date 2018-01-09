ec5e96782746696f241c96e838906dae51c59d0e
Thorsten Wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2018-01-09 10:37:05 +0100

    write box files

--- BoxWriter.py
+++ BoxWriter.py
@@ -11,4 +11,4 @@ def write_box(path, image, boxes,boxsize):
         for box in boxes:
             x_ll = int((box.x - boxsize / 2) * image.shape[1]) # lower left
             y_ll = int((box.y - boxsize / 2) * image.shape[0]) # lower right
-            boxwriter.writerow([x_ll,y_ll,boxsize,boxsize])
+            boxwriter.writerow([x_ll,y_ll,boxsize* image.shape[1],boxsize* image.shape[0]])
