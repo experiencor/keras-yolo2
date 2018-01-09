3af0f3eca7f0686c3b284244406b3b27e8a1a8ca
Thorsten Wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2018-01-09 10:50:02 +0100

    write box files

--- BoxWriter.py
+++ BoxWriter.py
@@ -3,14 +3,12 @@
 # Write EMAN2 box files
 
 import csv
-import numpy as np
 
 def write_box(path, image, boxes,boxsize):
     with open(path, 'wb') as boxfile:
         boxwriter = csv.writer(boxfile, delimiter='\t',
                            quotechar='|', quoting=csv.QUOTE_NONE)
         for box in boxes:
-            x_ll = np.int((box.x - boxsize / 2) * image.shape[1]) # lower left
-            y_ll = np.int((box.y - boxsize / 2) * image.shape[0]) # lower right
-            boxsize = np.int(boxsize * image.shape[1])
-            boxwriter.writerow([x_ll,y_ll,boxsize,boxsize])
+            x_ll = int((box.x - boxsize / 2) * image.shape[1]) # lower left
+            y_ll = int((box.y - boxsize / 2) * image.shape[0]) # lower right
+            boxwriter.writerow([x_ll,y_ll,boxsize* image.shape[1],boxsize* image.shape[0]])
