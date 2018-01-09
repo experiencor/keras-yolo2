fef4ac36464ade1deaedf2ea2181b20ccefae874
Thorsten Wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2018-01-09 10:34:35 +0100

    write box files

--- BoxWriter.py
+++ BoxWriter.py
@@ -5,9 +5,10 @@
 import csv
 
 def write_box(path, image, boxes,boxsize):
-    boxwriter = csv.writer(path, delimiter='\t',
+    with open(path, 'wb') as boxfile:
+        boxwriter = csv.writer(boxfile, delimiter='\t',
                            quotechar='|', quoting=csv.QUOTE_NONE)
-    for box in boxes:
-        x_ll = int((box.x - boxsize / 2) * image.shape[1]) # lower left
-        y_ll = int((box.y - boxsize / 2) * image.shape[0]) # lower right
-        boxwriter.writerow([x_ll,y_ll,boxsize,boxsize])
+        for box in boxes:
+            x_ll = int((box.x - boxsize / 2) * image.shape[1]) # lower left
+            y_ll = int((box.y - boxsize / 2) * image.shape[0]) # lower right
+            boxwriter.writerow([x_ll,y_ll,boxsize,boxsize])
